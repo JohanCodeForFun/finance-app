26a0d5306a8dec966ef1dea178ca2a553090a062
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-19 21:29:36 +0100

    object and arrays

--- sandbox.ts
+++ sandbox.ts
@@ -15,5 +15,24 @@ const circ = (diameter: number) => {
 	return diameter * Math.PI;
 };
 
-console.log(circ('hello'));
+// console.log(circ('hello'));
 console.log(circ(14));
+
+
+// arrays
+let numbers = [1, 1, 2, 3, 5, 8, 13];
+numbers.push(21);
+numbers.push('gfhf');
+numbers[0] = 1;
+numbers[0] = 'hello';
+
+// objects
+const johan = {
+	age: 38,
+	skills: ['JavaScript', 'TypeScript', 'Vue', 'Coach'],
+	location: 'Stocholm'
+}
+
+johan.age = 22
+johan.skills = 'fishing'
+
