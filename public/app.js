416d786c59215ddb7fb2227ec2370504fbcfd734
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-20 13:15:56 +0100

    rendering html

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplates.js';
 import { Payment } from './classes/Payment.js';
 console.log('hello');
 // let docOne: HasFormatter;
@@ -15,6 +16,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instanc
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -24,5 +28,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
